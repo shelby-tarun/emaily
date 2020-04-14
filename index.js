30805f6598b1665859efc040acba48f721e86ee2
Tarun Gupta <tarun.gupta@jungleworks.com>
2020-04-14 13:34:23 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send({ hi: 'there' });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT);
+
+
+/**
+ * In Package.json we have defined the engines for Herouku so that
+ * it can run our app with specified verisons
+ */
